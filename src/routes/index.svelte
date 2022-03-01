1d286213d6883c18b10c18788b23ef01d4f89c67
tawanasakuinje <sakuinjet@gmail.com>
2022-03-01 14:14:25 +0530

    more added

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,7 @@
  <div><img class="h-64 mt-4" src="/src/teka.png " alt=""></div> 
 </div>
 
-<div class='mt-20 text-center text-4xl p-4 text-gray-400'>
+<div class='mt-20 text-center text-4xl p-4 text-gray-600'>
    we supply high quilty and cost effective technolgy to africa 
 </div>
 
